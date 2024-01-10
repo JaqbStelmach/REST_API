00fd26d60e6e80e7be1724a7870621e351c91d7d
JaqbStelmach <140536806+JaqbStelmach@users.noreply.github.com>
2024-01-10 15:55:40 +0100

    Update GetBookingIdsTest.java

--- src/test/java/jakubstelmach/test/GetBookingIdsTest.java
+++ src/test/java/jakubstelmach/test/GetBookingIdsTest.java
@@ -3,7 +3,6 @@ package jakubstelmach.test;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import jakubstelmach.request.GetBookingIdsRequest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
